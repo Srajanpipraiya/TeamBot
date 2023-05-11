5a4ba32f78089d49a3904866c46a5f7984cbdc99
Srajanpipraiya <srajanpipraiya@gmail.com>
2023-05-11 12:50:18 +0530

    new_commit

---
+++ TeamBot/Hackx/Disease/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from Disease import views
+
+urlpatterns = [
+    path('',views.SignupPage,name='signup'),
+    path('login/',views.LoginPage,name='login'),
+    path('home/',views.HomePage,name='home'),
+    path('logout/',views.LogoutPage,name='logout'),
+    
+]
